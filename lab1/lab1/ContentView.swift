4c1b7961adce2f877dce55306a202c8ec2544025
lesolas20 <lesolas@proton.me>
2026-09-11 11:03:38 +0300

    Update the Hello World program

--- lab1/lab1/ContentView.swift
+++ lab1/lab1/ContentView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "star")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("HELLO")
         }
         .padding()
     }
