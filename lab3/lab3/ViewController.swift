8c231cdfed1ce57d78dd5c1bdf81d5499cea230a
lesolas20 <lesolas@proton.me>
2026-09-18 11:36:45 +0300

    Lab 3: WIP

--- lab3/lab3/ViewController.swift
+++ lab3/lab3/ViewController.swift
@@ -1,12 +1,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        var state: String
+        if sender.isOn {
+            state="ON"
+        } else {
+            state="OFF"
+        }
+        print("Switch got toggled \(state)")
+    }
+    
 }
-
